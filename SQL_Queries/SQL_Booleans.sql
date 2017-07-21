8eee94182c5f874bca7037351aa0f04f3c3cc046
Matt Stammers <matthewstammers@googlemail.com>
2017-07-21 07:11:57 +0100

    Update SQL_Booleans.sql

--- SQL_Queries/SQL_Booleans.sql
+++ SQL_Queries/SQL_Booleans.sql
@@ -1,2 +1,9 @@
 SELECT * FROM students WHERE IsActive;
 SELECT * FROM students WHERE IsActive = 1
+SELECT * FROM students WHERE tuition_received = 'F'
+
+-- or
+
+select *
+from students
+where not tuition_received;
