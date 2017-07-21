9b2326929c5eb26bed3db096a06890f7d7da0163
Matt Stammers <matthewstammers@googlemail.com>
2017-07-21 08:48:17 +0100

    Create INSERT_into.sql

---
+++ SQL_Queries/INSERT_into.sql
@@ -0,0 +1,9 @@
+INSERT INTO participants VALUES ('Matt', '26', TRUE);
+
+SELECT * FROM participants;
+
+# OR
+
+INSERT INTO participants (name, age, attending) VALUES ('Name', 22, TRUE);
+
+SELECT * FROM participants;
